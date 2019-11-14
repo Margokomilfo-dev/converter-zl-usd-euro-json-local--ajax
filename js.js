8648d83728b1cbec29837a7ded219fe1de3931bb
Margo <margokomilfo@mail.ru>
2019-11-14 13:03:24 +0100

    add jpg

--- js.js
+++ js.js
@@ -1,25 +1,49 @@
 'use strict';
 //конвертор валют со злотого на доллар
-let inputZl = document.getElementById("zl"),
-    inputUsd = document.getElementById("usd"),
-    inputEuro = document.getElementById("euro");;
 
-inputZl.addEventListener("input", () => {
-  let request = new XMLHttpRequest();
-  request.open("GET", "current.json");
-  request.setRequestHeader("Content-type", "application/json, charset = utf-8" );
-  request.send();
+  let inputZl = document.getElementById("zl"),
+      inputUsd = document.getElementById("usd"),
+      inputEuro = document.getElementById("euro"),
+      currentInp1 = document.getElementById("cur_in1"),
+      currentInp2 = document.getElementById("cur_in2"),
+      request,
+      data;
+//-----------------------------------------
+  document.addEventListener("DOMContentLoaded", () => {
+    request = new XMLHttpRequest();
+    request.open("GET", "current.json");
+    request.setRequestHeader("Content-type", "application/json, charset = utf-8" );
+    request.send();
   
+    request.addEventListener("readystatechange", function(){
+        data = JSON.parse(request.response);
+        currentInp1.value = data.usd;
+        currentInp2.value = data.euro;
+    });
+  });
+
+//-------------------------------------------
+  inputZl.addEventListener("input", () => {
+    request = new XMLHttpRequest();
+    request.open("GET", "current.json");
+    request.setRequestHeader("Content-type", "application/json, charset = utf-8" );
+    request.send();
 
     request.addEventListener("readystatechange", function(){
-      if (request.readyState === 4 && request.status == 200) {
-        let data = JSON.parse(request.response);
+      if (request.readyState === 4 && request.status == 200 && !isNaN(parseInt(inputZl.value)) && inputZl.value != ''){
+        data = JSON.parse(request.response);
+        inputUsd.value = (inputZl.value / data.usd).toFixed(2);
+        inputEuro.value = (inputZl.value / data.euro).toFixed(2);
 
-        inputUsd.value = inputZl.value / data.usd;
-        inputEuro.value = inputZl.value / data.euro;
+      } else if (inputZl.value == '') {
+        inputUsd.value = "0";
+        inputEuro.value = "0";
       } else {
         inputUsd.value = "problems...";
         inputEuro.value = "problems...";
       }
     });
-});
+  }); 
+
+    
+  
