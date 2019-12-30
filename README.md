56ae195f2fe5d31e9ceff852b960de9cfb06eb4d
Margo <margokomilfo@mail.ru>
2019-12-30 11:19:08 +0100

    js mod

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # converter-zl-usd-json-local--ajax
+RU -------
 Самый простой конвертор валют из Zł(PLN) в USD и Euro с помощью AJAX и локального сервера JSON
-
-intrex.html 
-js.js
-current.json (наш локальный сервер)
-- указаны текущие данные курса валюты (не забывайте самостоятельно их менять)
-
-
+1)intrex.html 
+2)js.js
+3)current.json (наш локальный сервер) - указаны текущие данные курса валюты (не забывайте самостоятельно их менять)
 Все файлы должны находиться в общей папке. Иначе следует исправить пути к файлам
  1. <script src="js.js"></script> (index.html)
  2. request.open("GET", "current.json"); (js.js)
 
+EN -------
+The largest currency converter from Zl to USD and Euro with help AJAX and local server JSON
+1)intrex.html 
+2)js.js
+3)current.json (local server) - current data of rate (don't forget to change them yourself)
+All files are in the common folder.
+ 1. <script src="js.js"></script> (index.html)
+ 2. request.open("GET", "current.json"); (js.js)
 
-ДОРАБАТЫВАЕТСЯ!
- - Read.txt на английском языке
+PL -------
+Najprostrzy przelicznik walut Zł -> USD i Euro z pomocą AJAX i lokalnego serweru JSON
+1)intrex.html 
+2)js.js
+3)current.json (lokalny serwer) - aktualne dane dotyczące kursu walutowego (nie zapomnij ich zmienić samodzielnie)
+Wszystkie pliki muszą znajdować się w folderze współdzielonym. W przeciwnym razie ścieżki plików powinny zostać poprawione.
+ 1. <script src="js.js"></script> (index.html)
+ 2. request.open("GET", "current.json"); (js.js)
--- js.js
+++ js.js
@@ -8,33 +8,31 @@
       currentInp2 = document.getElementById("cur_in2"),
       request,
       data;
+
+  let req = () => {
+      request = new XMLHttpRequest();
+      request.open("GET", "current.json");
+      request.setRequestHeader("Content-type", "application/json, charset = utf-8" );
+      request.send();
+    }
 //-----------------------------------------
   document.addEventListener("DOMContentLoaded", () => {
-    request = new XMLHttpRequest();
-    request.open("GET", "current.json");
-    request.setRequestHeader("Content-type", "application/json, charset = utf-8" );
-    request.send();
-  
-    request.addEventListener("readystatechange", function(){
+    req();
+    request.onreadystatechange = function() {
         data = JSON.parse(request.response);
         currentInp1.value = data.usd;
         currentInp2.value = data.euro;
-    });
+    };
   });
 
 //-------------------------------------------
   inputZl.addEventListener("input", () => {
-    request = new XMLHttpRequest();
-    request.open("GET", "current.json");
-    request.setRequestHeader("Content-type", "application/json, charset = utf-8" );
-    request.send();
-
-    request.addEventListener("readystatechange", function(){
+    req();
+    request.onreadystatechange = () => {
       if (request.readyState === 4 && request.status == 200 && !isNaN(parseInt(inputZl.value)) && inputZl.value != ''){
         data = JSON.parse(request.response);
         inputUsd.value = (inputZl.value / data.usd).toFixed(2);
         inputEuro.value = (inputZl.value / data.euro).toFixed(2);
-
       } else if (inputZl.value == '') {
         inputUsd.value = "0";
         inputEuro.value = "0";
@@ -42,8 +40,35 @@
         inputUsd.value = "problems...";
         inputEuro.value = "problems...";
       }
-    });
+    };
   }); 
 
-    
+
+  // inputZl.addEventListener("input", () => {
+  //   let p = new Promise((resolve, reject) => {
+  //     req();
+  //     request.onreadystatechange = () => {
+  //       if (request.readyState === 4 && request.status == 200 && !isNaN(parseInt(inputZl.value)) && inputZl.value != ''){
+  //         resolve()
+  //       } else if (inputZl.value == '') {
+  //         resolve()
+  //       } else {
+  //         reject()
+  //       }
+  //     };
+  //   });
+
+  //   p.then(() => {
+  //     data = JSON.parse(request.response);
+  //     inputUsd.value = (inputZl.value / data.usd).toFixed(2);
+  //     inputEuro.value = (inputZl.value / data.euro).toFixed(2);
+  //   }).then(() => {
+  //     inputUsd.value = "0";
+  //     inputEuro.value = "0";
+  //   }).catch(() => {
+  //     inputUsd.value = "problems...";
+  //     inputEuro.value = "problems...";
+  //   })
+  // }); 
+
   
